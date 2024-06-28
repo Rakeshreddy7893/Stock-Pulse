268d115e3620d294e895dd95e3fb9257b0f2945b
Rakeshreddy7893 <rakeshreddykalagiri@gmail.com>
2024-06-28 14:58:45 +0530

    Completed Awards and Hero component design

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,7 +2,19 @@
 
 import React from "react";
 function Hero() {
-  return <h1>Hero</h1>;
+  return (
+    <div className="container p-5 mb-5">
+
+      <div className="row test-center">
+        <img src="media/images/homeHero.png" alt="Hero iMAGES" className="mb-5"/>
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>Onmine platform to invest in stocks,derivation,mutual funds and more</p>
+        <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:'20%',margin:"0 auto"}}>Signup Now</button>
+      </div>
+
+
+    </div>
+  );
 }
 
 export default Hero;
